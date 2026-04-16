5938aac9d50c03b39e6c9b5b3afe08cababb15d8
Sam A <samarnerich@hotmail.co.nz>
2026-04-16 20:38:17 +1200

    Move extra function from settings component to utils. Add tests for settings component

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { defaultDaySettings } from "./defaults";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -9,4 +10,15 @@ export function timeToMinutes(time?: string | null): number | null {
   if (!time) return null;
   const [h, m] = time.slice(0, 5).split(":").map(Number);
   return h * 60 + m;
+}
+
+export function dowDefault(
+  day: string,
+  settings: Record<string, string> | null,
+) {
+  const result =
+    settings?.[day] !== undefined
+      ? settings[day] === "true"
+      : defaultDaySettings[day];
+  return result;
 }
