bd9a6e7df601fe9308b426a76d97c28013d6dacf
Sam A <samarnerich@hotmail.co.nz>
2026-04-23 21:36:26 +1200

    Add utils test cases

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { defaultDaySettings } from "./defaults";
+import { defaultDaySettings } from "@/lib/defaults";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
