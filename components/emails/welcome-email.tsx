492a0b2e2f8579bfbfcf01719d885512f7f55d46
Sam A <samarnerich@hotmail.co.nz>
2026-06-09 15:00:09 +1200

    Update doco and example compose to include email. Fill out welcome email template some more

--- components/emails/welcome-email.tsx
+++ components/emails/welcome-email.tsx
@@ -8,6 +8,17 @@ export function WelcomeEmail({ name }: EmailTemplateProps) {
   return (
     <div>
       <h1>Welcome to Tempus, {name}!</h1>
+      <br />
+      <p>You're all signed up!</p>
+      <p>
+        Tempus is a timetable app, designed to give you your timetable at a
+        glance, wherever you are.
+      </p>
+      <p>
+        To get started, simply log in to your account and start loading your
+        timetable.
+      </p>
+      <p>Happy timekeeping!</p>
     </div>
   );
 }
